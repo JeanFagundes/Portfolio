7e72503afa8be353e3feaf374e4fcd6d69903201
JeanFagundes <jean-cabral@hotmail.com>
2023-10-05 23:54:12 -0300

    home page finalizada

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,43 @@
 import styles from './Footer.module.scss';
+import { AiFillGithub, AiFillLinkedin, AiOutlineInstagram } from 'react-icons/ai';
 
 export default function Footer() {
     return (
         <footer className={styles.container}>
-            <ul className={styles.container__firstList}>
-                <li>Inicio</li>
-                <li>Sobre</li>
-                <li>Tecnologias</li>
-                <li>Projetos</li>
-                <li>Contato</li>
-            </ul>
-            <p>Desenvolvido por Jean Fagundes</p>
+            <div className={styles.container__flexbox}>
+                <div className={styles.container__contact}>
+                    <ul className={styles.container__firstList}>
+                        <li>
+                            <p>11 95440-6674</p>
+                        </li>
+                        <li>
+                            <p>jean-cabral@hotmail.com</p>
+                        </li>
+                    </ul>
+                    <ul className={styles.container__socialMidia}>
+                        <li>
+                            <AiFillGithub size={30} />
+                        </li>
+                        <li>
+                            <AiFillLinkedin size={30} />
+                        </li>
+                        <li>
+                            <AiOutlineInstagram size={30} />
+                        </li>
+                    </ul>
+                </div>
+
+                <div className={styles.container__flexBoxFooter}>
+                    <ul className={styles.container__secondList}>
+                        <li>Inicio</li>
+                        <li>Sobre</li>
+                        <li>Tecnologias</li>
+                        <li>Projetos</li>
+                        <li>Contato</li>
+                    </ul>
+                    <p>Desenvolvido por Jean Fagundes</p>
+                </div>
+            </div>
         </footer>
     );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,20 @@
 import About from 'components/About';
 import Projects from 'components/Projects';
 import TechStack from 'components/TechStack';
+import styles from './Home.module.scss';
 
 export default function Home() {
     return (
-        <div>
-            <About />
-            <TechStack />
-            <Projects />
+        <div className={styles.container}>
+            <div className={styles.container__about}>
+                <About />
+            </div>
+            <div className={styles.container__techStack}>
+                <TechStack />
+            </div>
+            <div className={styles.conainer__projects}>
+                <Projects />
+            </div>
         </div>
     );
 }
